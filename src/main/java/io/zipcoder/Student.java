3bd444b4a8b4d77819e19a89962d4916ef860b14
eliszewski <liszewski31098@gmail.com>
2022-11-08 14:10:02 -0500

    added some getters and setters

--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -1,4 +1,46 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Student {
+
+
+    public String firstName;
+    public String lastName;
+    public ArrayList<Double> examScores;
+
+    public Student(String firstName, String lastName, Double[] examScores){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.examScores = new ArrayList<>(Arrays.asList(examScores));
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public ArrayList<Double> getExamScores() {
+        return examScores;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setExamScores(ArrayList<Double> examScores) {
+        this.examScores = examScores;
+    }
+    public Integer getNumberOfExamsTaken(){
+        return examScores.size();
+    }
+
 }

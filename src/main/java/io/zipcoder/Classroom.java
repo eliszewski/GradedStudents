ccb947d8519ebf3683d1457a0a2ec38bc6ddd0e4
eliszewski <liszewski31098@gmail.com>
2022-11-08 16:21:18 -0500

    add some classroom constructors and methods

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -2,5 +2,27 @@ package io.zipcoder;
 
 public class Classroom {
     Student[] students;
+    public Classroom(int maxNumberOfStudents){
+        students = new Student[maxNumberOfStudents];
+    }
+
+    public Classroom(Student[] students) {
+        this.students = students;
+    }
+    public Classroom(){
+        this.students = new Student[30];
+    }
+    public Student[] getStudents(){
+        return this.students;
+    }
+
+    public Double getAverageExamScore(){
+        Double sum = 0.0;
+        for (Student student: this.students
+             ) {
+            sum+= student.getAverageExamScore();
+        }
+        return sum / this.students.length;
+    }
 
 }

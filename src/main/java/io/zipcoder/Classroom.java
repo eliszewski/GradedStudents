40864df57581daefa110213c97b535a75c5a19e0
Eryk <eryk@Zipcoders-MacBook-Pro-71.local>
2022-11-09 18:16:07 -0500

    add getGradeBook

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -27,24 +27,26 @@ public class Classroom {
         return sum / this.students.length;
     }
     public void addStudent(Student student){
-        List<Student> students1 = Arrays.asList(this.students);
+        List<Student> students1 = new ArrayList<>(Arrays.asList(this.students));
         students1.add(student);
         this.students = students1.toArray(new Student[0]);
     }
     public void removeStudent(String firstName, String lastName){
-        List<Student> students1 = Arrays.asList(this.students);
+        List<Student> students1 = new ArrayList<>(Arrays.asList(this.students));
+        Student studentToRemove = new Student(null,null,null);
         for (Student student:students1
              ) {
-            if(student.getFirstName().equals(firstName) && student.getLastName().equals(lastName)){
-                students1.remove(student);
+            if(student != null && (student.getFirstName().equals(firstName) && student.getLastName().equals(lastName))){
+                studentToRemove = student;
             }
         }
+        students1.remove(studentToRemove);
         this.students = students1.toArray(new Student[0]);
     }
     protected static class StudentComparator implements Comparator<Student> {
 
         @Override
-        public int compare(Student o1, Student o2) {
+        public int compare(Student o1, Student o2) { //sorting algorithm
             if(o1.getAverageExamScore() > o2.getAverageExamScore()) {
                 return -1;
             }else if (o1.getAverageExamScore() < o2.getAverageExamScore()) {
@@ -57,10 +59,36 @@ public class Classroom {
         }
 
     }
-    public Student[] getStudentsByScore(){
+    public Student[] getStudentsByScore() {
         List<Student> theList = new ArrayList(Arrays.asList(this.getStudents()));
         Collections.sort(theList, new StudentComparator());
         return theList.toArray(new Student[0]);
     }
 
+    public double percentileCalculator(int index, int len){
+        return Math.abs((index + 1) - len) / len * 100;
+    }
+    public Map<Student,Character> getGradeBook(){
+        Map<Student,Character> mapping = new LinkedHashMap<>();
+        Student[] sortedStudents = this.getStudentsByScore();
+        int n = sortedStudents.length;
+        for(int i = 0;i<n;i++){
+            if(percentileCalculator(i,n)>=90){
+                mapping.put(sortedStudents[i],'A');
+            }
+            if(percentileCalculator(i,n)<90 && percentileCalculator(i,n)>=70){
+                mapping.put(sortedStudents[i],'B');
+            }
+            if(percentileCalculator(i,n)<70 && percentileCalculator(i,n)>=50){
+                mapping.put(sortedStudents[i],'C');
+            }
+            if(percentileCalculator(i,n)<50 && percentileCalculator(i,n) >=10){
+                mapping.put(sortedStudents[i],'D');
+            }
+            if(percentileCalculator(i,n) <10){
+                mapping.put(sortedStudents[i],'F');
+            }
+        }
+        return mapping;
+    }
 }
--- src/test/java/io/zipcoder/ClassroomTest.java
+++ src/test/java/io/zipcoder/ClassroomTest.java
@@ -3,6 +3,8 @@ package io.zipcoder;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Arrays;
+
 public class ClassroomTest {
     @Test
     public void testGetStudentsByScore(){
@@ -20,4 +22,69 @@ public class ClassroomTest {
         Student [] expected = {s2,s3,s1};
         Assert.assertArrayEquals(expected,actual);
     }
+    @Test
+    public void testConstructorWithMax(){
+        int max = 30;
+        Classroom testClass = new Classroom(30);
+        Assert.assertEquals(max,testClass.getStudents().length);
+    }
+    @Test
+    public void testConstructorWithArray(){
+        Student[] studentArr = new Student[0];
+        Classroom testClass = new Classroom(studentArr);
+        Assert.assertEquals(testClass.getStudents(),studentArr);
+    }
+    @Test
+    public void testNullaryConstructor(){
+        int len = 30;
+        Classroom testClass = new Classroom();
+        Assert.assertEquals(testClass.getStudents().length, len);
+    }
+    @Test
+    public void testGetStudents(){
+        Double[] s1Scores = {10.0,15.0,5.0};
+        Student s1 = new Student("John","K",s1Scores);
+        Double[] s2Scores = {10.0,30.0,20.0};
+        Student s2 = new Student("Kevin", "P",s2Scores);
+        Student s3 = new Student("Alex", "L",s1Scores);
+        Student [] test = {s1,s2,s3};
+        Classroom testClass = new Classroom(test);
+        Assert.assertArrayEquals(test,testClass.getStudents());
+    }
+    @Test
+    public void testGetAverageExamScore(){
+        Double[] s1Scores = {10.0,15.0,5.0};
+        Student s1 = new Student("John","K",s1Scores);
+        Double[] s2Scores = {40.0,40.0,40.0};
+        Student s2 = new Student("Kevin", "P",s2Scores);
+        Student s3 = new Student("Alex", "L",s1Scores);
+        Student [] test = {s1,s2,s3};
+        Classroom testClass = new Classroom(test);
+
+        Double expectedAvg = 20.0;
+        Assert.assertEquals(testClass.getAverageExamScore(),expectedAvg);
+    }
+    @Test
+    public void testAddStudent(){
+        Student test = new Student("Kevin","Kevin?",null);
+        Classroom tester = new Classroom();
+        tester.addStudent(test);
+        Assert.assertEquals(tester.getStudents()[tester.getStudents().length - 1],test);
+    }
+    @Test
+    public void testRemoveStudent(){
+        String first = "Kevin";
+        String last = "Kevin?";
+        Student test = new Student(first,last,null);
+        Classroom tester = new Classroom();
+        tester.addStudent(test); //add to the back
+        tester.removeStudent(first,last); //remove from the back
+        Assert.assertNotEquals(tester.getStudents()[tester.getStudents().length - 1], test); //should not be in the back
+    }
+    @Test
+    public void testGetGradeBook(){
+
+
+    }
+
 }
